3fad9488bb80af92db4f3e912f34a0f3e6ae37e1
enikma-mod <sirmodika@gmmail.com>
2022-07-08 11:59:39 +0200

    If-else challenge

---
+++ Python/If_else.py
@@ -0,0 +1,32 @@
+# Python If-Else
+
+# Task 
+# Given an integer, n, perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+# Input Format
+# A single line containing a positive integer, n.
+
+# Constraints
+# 1 <= n <= 100
+
+# Output Format
+# Print Weird if the number is weird; otherwise, print Not Weird.
+
+#!/bin/python
+import sys
+
+
+number = int(input("Enter your number: ").strip())
+
+if (number % 2 != 0):
+    print("Weird")
+elif (number >= 2 and number <= 5):
+    print("Not Weird")
+elif (number >= 6 and number <= 20):
+    print("Weird")
+else:
+    print("Not Weird")
